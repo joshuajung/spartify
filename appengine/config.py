19892037f7fad3167f0db1e60334f02e990cba6e
Joshua Jung <mail@joshua-jung.com>
2014-12-14 10:48:52 +0100

    Erstes aufräumen.

--- appengine/config.py
+++ appengine/config.py
@@ -1,11 +1,13 @@
+# ?
 USER_REPEAT_VOTE_WAIT = 600
-# 48 hours
 CACHE_DEFAULT_TIMEOUT = 172800
+
+# Wie viele Songs muessen mindestens enthalten sein, bevor Spartify selbst auffuellt?
 PARTY_QUEUE_PANIC = 4
 
+# API-Konfiguration
 ECHONEST_API_KEY = 'RVA3NJIGEZR6AQL1U'
 ECHONEST_BASE_URL = 'http://developer.echonest.com/api/v4/'
 ECHONEST_MAX_TRACKS_PER_ARTIST = 1
 ECHONEST_MAX_SIMILAR_ARTIST = 3
-
 SPOTIFY_BASE_URL = 'http://ws.spotify.com/'
